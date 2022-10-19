5cddae3d29aeb70fd22f608003c788ac2cc887f8
Pinoydatascientist <ronrickarnaiz@gmail.com>
2022-10-19 13:35:50 -0400

    First Commit

---
+++ hyperparameter_optimization.py
@@ -0,0 +1,6 @@
+"""Optimizes hyperparameters using Bayesian optimization."""
+
+from servier.hyperparameter_optimization import servier_hyperopt
+
+if __name__ == '__main__':
+    servier_hyperopt()
---
+++ interpret.py
@@ -0,0 +1,7 @@
+"""Uses MCTS to interpret a servier model."""
+
+from servier.interpret import servier_interpret
+
+
+if __name__ == '__main__':
+    servier_interpret()
