338dc2e245a433442f36fa50cbbe8f65318f6185
Pinoydatascientist <ronrickarnaiz@gmail.com>
2022-10-20 12:06:19 -0400

    Delete fingerprint.py

--- fingerprint.py
+++
@@ -1,7 +0,0 @@
-"""Loads a trained chemprop model checkpoint and encode latent fingerprint vectors for the molecules in a dataset.
-    Uses the same command line arguments as predict."""
-
-from servier.train import chemprop_fingerprint
-
-if __name__ == '__main__':
-    chemprop_fingerprint()
